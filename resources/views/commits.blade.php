b0baf924f2f5891160ac07c088e4901be24889aa
deavila747 <cristiandeavila747@gmail.com>
2023-11-23 19:47:30 -0500

    Landin Page Basica Subscribe

---
+++ resources/views/commits.blade.php
@@ -0,0 +1,15 @@
+<p>Hola this a text for example  </p>
+<form class="footer__form"    action="https://formspree.io/f/mjvqdzlo"
+method="POST">
+    <h2 class="footer__newsletter">Preguntas Y Comentarios </h2>
+    <div class="footer__inputs">
+        <input type="email" placeholder="Email:" class="footer__input" name="_replyto">
+        <label>
+           
+            <textarea name="message" placeholder="comentarios:"></textarea>
+          </label>
+          <br>
+          <br>
+        <input type="submit" value="Comentar !!!" class="footer__submit">
+    </div>
+</form>
